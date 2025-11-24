b8b899613c3e977da216aae675cd5b3993002a89
LancelotTC <lancelot.tariot.camille@gmail.com>
2025-11-24 16:36:01 +0100

    Added main.py

--- feistel_implementation.py
+++ feistel_implementation.py
@@ -36,16 +36,3 @@ def feistel_decrypt(
         L, R = feistel_round(L, R, k, f)
 
     return (L << 32) | R
-
-
-# Example
-keys = [0x11111111, 0x22222222, 0x33333333, 0x44444444]
-plaintext = 0xDEADBEEFCAFEBABE
-
-
-cipher = feistel_encrypt(plaintext, keys)
-plain2 = feistel_decrypt(cipher, keys)
-
-print(hex(plaintext))
-print(hex(cipher))
-print(hex(plain2))
---
+++ main.py
@@ -0,0 +1,13 @@
+from feistel_implementation import feistel_encrypt, feistel_decrypt
+
+# Example
+keys = [0x11111111, 0x22222222, 0x33333333, 0x44444444]
+plaintext = 0xDEADBEEFCAFEBABE
+
+
+cipher = feistel_encrypt(plaintext, keys)
+plain2 = feistel_decrypt(cipher, keys)
+
+print(hex(plaintext))
+print(hex(cipher))
+print(hex(plain2))
